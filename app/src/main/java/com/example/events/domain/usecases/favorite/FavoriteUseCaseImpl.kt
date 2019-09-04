3c6c63f3938464ed3119496ea7d514d75abb2ce4
festus <kiamfestoh@gmail.com>
2019-09-04 23:04:54 +0300

    edit layouts

--- app/src/main/java/com/example/events/domain/usecases/favorite/FavoriteUseCaseImpl.kt
+++ app/src/main/java/com/example/events/domain/usecases/favorite/FavoriteUseCaseImpl.kt
@@ -7,7 +7,7 @@ import com.example.events.domain.repository.IFavoriteRepository
  * Created by Festus Kiambi on 9/4/19.
  */
 class FavoriteUseCaseImpl(private val favoriteRepository: IFavoriteRepository): IFavoriteUseCase {
-    override suspend fun getFavorites(): List<Favorite> {
+    override suspend fun getFavorites(): List<Favorite>? {
 
         return favoriteRepository.getFavorites()
     }
---
+++ app/src/main/java/com/example/events/listevents/ui/ListEventsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.events.listevents.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.events.R
+
+class ListEventsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_events)
+    }
+}
