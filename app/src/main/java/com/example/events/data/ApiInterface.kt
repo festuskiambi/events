8ef7f0fc4d4214ac8c5c9fe52db1d17a7983e9a2
festus <kiamfestoh@gmail.com>
2019-09-04 21:50:08 +0300

    add apiinterface

---
+++ app/src/main/java/com/example/events/data/ApiInterface.kt
@@ -0,0 +1,26 @@
+package com.example.events.data
+
+import com.example.events.domain.entities.Category
+import com.example.events.domain.entities.Event
+import com.example.events.domain.entities.Favorite
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+/**
+ * Created by Festus Kiambi on 9/4/19.
+ */
+interface ApiInterface {
+
+    @GET("events")
+    suspend fun getEvents(): Response<List<Event>>
+
+    @GET("events/{id}")
+    suspend fun getEventById(@Path("id") id: String): Response<Event>
+
+    @GET("favorites")
+    suspend fun getFavorites(): Response<List<Favorite>>
+
+    @GET("categories")
+    suspend fun getCategories(): Response<List<Category>>
+}
