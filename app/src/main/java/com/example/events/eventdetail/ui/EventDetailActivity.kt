f8189bc0d1ac739152325d4760bb5fdb667a1fd7
festus <kiamfestoh@gmail.com>
2019-09-06 00:36:42 +0300

    add toolbar to listevent activity

--- app/src/main/java/com/example/events/eventdetail/ui/EventDetailActivity.kt
+++ app/src/main/java/com/example/events/eventdetail/ui/EventDetailActivity.kt
@@ -3,11 +3,25 @@ package com.example.events.eventdetail.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.events.R
+import com.example.events.domain.entities.Event
 
 class EventDetailActivity : AppCompatActivity() {
+
+    lateinit var event: Event
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_detail)
+
+        initView()
+    }
+
+    private fun initView() {
+
+        setupTicketTypeAdapter()
+    }
+
+    private fun setupTicketTypeAdapter() {
     }
 }
