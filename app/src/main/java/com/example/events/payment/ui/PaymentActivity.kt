aa3f544cd7dca348e3d5a907061524a44344dfd3
festus <kiamfestoh@gmail.com>
2019-09-06 10:34:56 +0300

    add on radio button checked

--- app/src/main/java/com/example/events/payment/ui/PaymentActivity.kt
+++ app/src/main/java/com/example/events/payment/ui/PaymentActivity.kt
@@ -3,11 +3,22 @@ package com.example.events.payment.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.events.R
+import kotlinx.android.synthetic.main.activity_payment.*
 
 class PaymentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
+
+        rb_card.setOnClickListener {
+            rb_card.isChecked = true
+            rb_mpesa.isChecked = false
+        }
+
+        rb_mpesa.setOnClickListener {
+            rb_mpesa.isChecked = true
+            rb_card.isChecked = false
+        }
     }
 }
